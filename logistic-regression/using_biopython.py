affb3759ed0b5da6584b995f043283e1d53f96dd
carlosp420 <mycalesis@gmail.com>
2015-10-19 10:28:08 +0300

    logistic regression over iris dataset using script

--- logistic-regression/using_biopython.py
+++ logistic-regression/using_biopython.py
@@ -1,2 +1,26 @@
 from Bio import LogisticRegression
-from sci
+import numpy as np
+
+
+all_data = np.loadtxt("../datasets/iris/iris.data", delimiter=",",
+                      dtype="float, float, float, float, S11")
+
+xs = []
+ys = []
+
+for i in all_data:
+    if 'virgi' not in str(i[-1]):
+        xs.append([i[0], i[1], i[2], i[3]])
+        if 'setosa' in str(i[-1]):
+            ys.append(0)
+        else:
+            ys.append(1)
+
+test_xs = xs.pop()
+test_ys = ys.pop()
+
+def show_progress(iteration, loglikelihood):
+    print("Iteration:", iteration, "Log-likelihood function:", loglikelihood)
+
+model = LogisticRegression.train(xs, ys, update_fn=show_progress)
+print("This should be Iris-versic (1): {}".format(LogisticRegression.classify(model, test_xs)))
